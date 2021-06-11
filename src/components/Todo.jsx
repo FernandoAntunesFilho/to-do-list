f32e38c077ed3dcac0aafe9b0e1d269fd449b363
Fernando Antunes <fernando.antunes1@gmail.com>
2021-06-11 16:24:33 -0300

    renderizando lista de tarefas a fazer com base no local storage

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,14 +2,34 @@ import React from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todo: ['Tarefa 1', 'Tarefa 2', 'Tarefa 3'],
+      // done: ['Tarefa 4', 'Tarefa 5'],
+    };
+  }
+
+  componentDidMount() {
+    this.tastksList();
+  }
+
+  tastksList() {
+    const { todo } = this.state;
+    todo.forEach((task) => {
+      const todoList = document.querySelector('.todo-list');
+      const p = document.createElement('p');
+      p.innerText = task;
+      todoList.appendChild(p);
+    });
+  }
+
   render() {
     return (
       <div>
         <h3>A fazer</h3>
-        <ul>
-          <li>Tarefa 1</li>
-          <li>Tarefa 2</li>
-        </ul>
+        <div className="todo-list" />
       </div>
     );
   }
